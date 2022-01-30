9c0038890346a910ff1d317b54c3717e90ae4b75
Wenxi Zhang <wexzhang@ucdavis.edu>
2022-01-30 15:36:19 -0800

    add roles

--- R/gen_missing_val.R
+++ R/gen_missing_val.R
@@ -1,6 +1,17 @@
-# generate m % of missing values
-gen_missing_val <- function(data,seed,m){
+#' generate a_percent % of missing values
+#' 
+#'given a complete data set, generate a specific percentage of missing values
+#' 
+#' @param data input complete data, only numeric values
+#' @param seed Set the random seed
+#' @param na_percent percentage of missing values user intend to generate 
+#'
+#' @return dataframe with na_percent% missing values
+#' 
+#'   
+#'@export
+gen_missing_val <- function(data,seed,na_percent){
   set.seed(seed)
-  datam <- as.data.frame(lapply(data, function(cc) cc[ sample(c(TRUE, NA), prob = c(1-m, m), size = length(cc), replace = TRUE) ]))
+  datam <- as.data.frame(lapply(data, function(cc) cc[ sample(c(TRUE, NA), prob = c(1-na_percent, na_percent), size = length(cc), replace = TRUE) ]))
   return(datam)
 }
