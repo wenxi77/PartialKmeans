808cd7d4cda1e7651df7be24cc1cb7de8dc7655a
Wenxi Zhang <wexzhang@ucdavis.edu>
2022-01-30 17:39:45 -0800

    add docs

--- R/test_accuracy.R
+++ R/test_accuracy.R
@@ -1,3 +1,13 @@
+#' test accuracy rate of kmeans clustering fitted values
+#' 
+#' @param actual_y actual labels/clusters each row of data set from
+#' @param fit_y fitted value of each row of data set
+#'
+#' @return  accuracy rate of given sets of data
+#' 
+#' }
+#' 
+#'@export
 test_accuracy<-function(actual_y,fit_y){
   acc_rate<-sum(fit_y==actual_y,na.rm = TRUE)/length(fit_y)
   return(acc_rate)
