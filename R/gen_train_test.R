cd3070cd2ed1194ab4fa9fe56629812124f00a1e
Wenxi Zhang <wexzhang@ucdavis.edu>
2022-01-30 17:22:36 -0800

    add docs

--- R/gen_train_test.R
+++ R/gen_train_test.R
@@ -1,5 +1,20 @@
-# train test split
-#return $train, $test
+
+#' train test split
+#' 
+#'given a whole data set, split into training and testing data
+#' 
+#' @param data input data matrix, only numeric values or NAs
+#' @param seed Set the random seed
+#'
+#' @return  
+#' \describe{
+#'   \item{Train}{training set}
+#'   \item{test}{testing set}
+#'   
+#' }
+#' 
+#'   
+#'@export
 gen_train_test <- function(data,seed) {
   set.seed(seed)
   trainIndex <- createDataPartition(1:dim(data)[1], p = .7,
