2fe7fa650aa470e4ce64f0b8dcaa685fc980c8b3
Wenxi Zhang <wexzhang@ucdavis.edu>
2022-02-10 00:31:06 -0800

    exclude piping

--- R/get_initC.R
+++ R/get_initC.R
@@ -12,6 +12,6 @@
 #'@export
 gen_initC <- function(xdata,n_cluster){
   initC <- apply(xdata,2, function(x){x[sample(complete.cases(x) %>% which(),n_cluster)]})
-  initc <- as.data.frame(initC) %>% unlist() %>%as.numeric()
+  initc <- as.numeric(unlist(initC))
   return(initc)
 }
--- R/plot_distance.R
+++ R/plot_distance.R
@@ -16,7 +16,6 @@
 #' 
 #'@export
 plot_distance <- function(model,x_data,plot_type){
-  library(dplyr)
   library(ggplot2)
   library(hrbrthemes)
   library(viridis)
@@ -24,14 +23,13 @@ plot_distance <- function(model,x_data,plot_type){
   na_per_row <- apply(x_data,1,function(x){sum((is.na(x)))})
   na_dis_per_row <- tibble(num_na=factor(na_per_row),dis_per_row)
   if (plot_type=="histogram"){
-    p <- na_dis_per_row %>%
-      ggplot( aes(x=dis_per_row, fill=num_na)) +
+    p <- ggplot(na_dis_per_row, aes(x=dis_per_row, fill=num_na)) +
       geom_histogram( color="#e9ecef", alpha=0.6, position = 'identity') +
       theme_ipsum() +
       labs(fill="")
   }
   if (plot_type=="density"){
-    p <- na_dis_per_row %>% ggplot( aes(x=dis_per_row, color=num_na,fill=num_na)) +
+    p <-  ggplot(na_dis_per_row,aes(x=dis_per_row, color=num_na,fill=num_na)) +
       geom_density(adjust=1.5, alpha=.4) +
       theme_ipsum()
   }
